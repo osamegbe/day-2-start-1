c19dda9c793ab8bde0658a87ce957c51f4d27eaf
osamehgbe <68432204+osamegbe@users.noreply.github.com>
2021-08-09 21:05:53 +0000

    subscript practice

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Data Types
+#Int, flotats, strings and boolians
+
+#string = string of characters strong together . needs double quotes
+
+#subscript - pulling out a particular element from a string. programming starts counting from zero
+print("Hello"[0])
