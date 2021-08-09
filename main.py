89018241d15cac93c3f6d5f894fcfd7e4a83a7b4
osamehgbe <68432204+osamegbe@users.noreply.github.com>
2021-08-09 21:07:22 +0000

    What the code is doing

--- main.py
+++ main.py
@@ -3,5 +3,8 @@
 
 #string = string of characters strong together . needs double quotes
 
-#subscript - pulling out a particular element from a string. programming starts counting from zero
-print("Hello"[0])
+#subscript - pulling out a particular element from a string. programming starts counting from zero 
+# use square brackets to pull out individuals characters
+print("Hello"[4])
+# if a number is in double quotes then its treated like regular text because of the double quotes 
+print("123")
