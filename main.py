cca7ad3f05f9e90fc06c78f26b512fd3b261b964
osamehgbe <68432204+osamegbe@users.noreply.github.com>
2021-08-09 21:10:33 +0000

    added definitions of string, float, booleans and Ints

--- main.py
+++ main.py
@@ -8,3 +8,13 @@
 print("Hello"[4])
 # if a number is in double quotes then its treated like regular text because of the double quotes 
 print("123")
+
+#Integer - for whole number. numbers without decimal places. just write the number without anything else
+# _ underscore are seens as comas inprogramming
+print (123 + 345)
+
+#float 0 for number that have decimal place
+
+print (3.123)
+
+#boolean - this has two possible values. either True of False - no quotation marks around them
